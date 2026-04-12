a38ab5f50547e509ad3fef9158c5ab0589145472
Daren Drahoš <45952291+Dazza9905@users.noreply.github.com>
2026-04-12 15:37:18 +0200

    Remove placeholder comment from user script
    Removed placeholder comment for user logic.

---
+++ script.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         AIS Stuba Enhancements
+// @namespace    github.com/Dazza9905/ais-stuba-theme
+// @version      1.0.0
+// @description  Enhancements for the STU AIS system.
+// @author       Dazza9905
+// @match        https://ais.stuba.sk/*
+// @updateURL    https://raw.githubusercontent.com/Dazza9905/ais-stuba-theme/main/script.user.js
+// @downloadURL  https://raw.githubusercontent.com/Dazza9905/ais-stuba-theme/main/script.user.js
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    console.log("AIS Stuba script loaded");
+})();
